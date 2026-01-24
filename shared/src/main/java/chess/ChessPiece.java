054ab017dc462fd3a7f346fcffbe1bb2209373ea
Clay Eldredge <clay.eldredge@gmail.com>
2026-01-23 19:15:35 -0700

    Do chesspiece moves. Took me 3 2.25 hours

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -2,7 +2,9 @@ package chess;
 
 import javax.naming.event.ObjectChangeListener;
 import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
+import java.util.ArrayList;
 
 /**
  * Represents a single chess piece
@@ -45,6 +47,153 @@ public class ChessPiece {
         return this.type;
     }
 
+    private boolean isInBounds(ChessPosition position) {
+        return ((position.getRow() >= 1)
+                && (position.getColumn() >= 1)
+                && (position.getRow() <= 8)
+                && (position.getColumn() <= 8));
+    }
+
+    private boolean isOccupied(ChessBoard board, ChessPosition pos) {
+        ChessPiece piece = board.getPiece(pos);
+        return (piece != null);
+    }
+
+    private ChessPosition getPositionFromVector(ChessBoard board, ChessPosition startPos, int drow, int dcol, int range) {
+        int rowdx = drow * range;
+        int coldx = dcol * range;
+        return new ChessPosition(startPos.getRow() + rowdx,startPos.getColumn() + coldx);
+    }
+
+    private Collection<ChessMove> getMovesInDirection(ChessBoard board, ChessPosition startPos, int drow, int dcol, int range) {
+        Collection<ChessMove> openMoves = new ArrayList<>();
+        for (int i = 1; i <= range; i++) {
+            boolean lastLoop = false;
+            ChessPosition targetPos = getPositionFromVector(board, startPos, drow, dcol, i);
+            ChessMove targetMove = new ChessMove(startPos, targetPos, null);
+            if (!(isInBounds(targetPos))) {
+                break;
+            }
+            if (isOccupied(board, targetPos)) {
+                ChessGame.TeamColor occupyingPieceColor = board.getPiece(targetPos).teamColor;
+                if (occupyingPieceColor.equals(this.getTeamColor())) {
+                    break;
+                } else {
+                    lastLoop = true;
+                }
+            }
+            openMoves.add(targetMove);
+            if (lastLoop) {
+                break;
+            }
+        }
+        return openMoves;
+    }
+
+    private Collection<ChessMove> getMovesInDirections(ChessBoard board, ChessPosition startPos, int range, int[][] directions) {
+        Collection<ChessMove> moves = new ArrayList<>();
+        for (int[] dir : directions) {
+            int dRow = dir[0];
+            int dCol = dir[1];
+            moves.addAll(getMovesInDirection(board, startPos, dRow, dCol, range));
+        }
+        return moves;
+    }
+
+    private Collection<ChessMove> getAllPromotionsForMove(ChessMove move) {
+        Collection<ChessMove> moves = new ArrayList<>();
+        moves.add(new ChessMove(move.getStartPosition(), move.getEndPosition(), PieceType.BISHOP));
+        moves.add(new ChessMove(move.getStartPosition(), move.getEndPosition(), PieceType.QUEEN));
+        moves.add(new ChessMove(move.getStartPosition(), move.getEndPosition(), PieceType.ROOK));
+        moves.add(new ChessMove(move.getStartPosition(), move.getEndPosition(), PieceType.KNIGHT));
+        return moves;
+    }
+
+    private Collection<ChessMove> getDiagnolPawnMove(ChessBoard board, ChessPosition startPos, ChessPosition targetPos) {
+        ChessPiece target = board.getPiece(targetPos);
+        if (target == null || target.getTeamColor().equals(this.getTeamColor())) {
+            return null;
+        } else {
+            ChessMove move = new ChessMove(startPos, targetPos, null);
+            if (targetPos.getRow() == 1 && this.getTeamColor().equals(ChessGame.TeamColor.BLACK)) {
+                return getAllPromotionsForMove(move);
+            }
+            if (targetPos.getRow() == 8 && this.getTeamColor().equals(ChessGame.TeamColor.WHITE)) {
+                return getAllPromotionsForMove(move);
+            }
+            Collection<ChessMove> moves = new ArrayList<>();
+            moves.add(move);
+            return moves;
+        }
+    }
+
+    private Collection<ChessMove> getStraightPawnMove(ChessBoard board, ChessPosition startPos, ChessPosition targetPos) {
+        ChessPiece target = board.getPiece(targetPos);
+        if (target == null) {
+            ChessMove move = new ChessMove(startPos, targetPos, null);
+            if (targetPos.getRow() == 1 && this.getTeamColor().equals(ChessGame.TeamColor.BLACK)) {
+                return getAllPromotionsForMove(move);
+            }
+            if (targetPos.getRow() == 8 && this.getTeamColor().equals(ChessGame.TeamColor.WHITE)) {
+                return getAllPromotionsForMove(move);
+            }
+            Collection<ChessMove> moves = new ArrayList<>();
+            moves.add(move);
+            return moves;
+        } else {
+            return null;
+        }
+    }
+
+    private Collection<ChessMove> getPawnMoves(ChessBoard board, ChessPosition startPos, int rowdx) {
+        Collection<ChessMove> moves = new ArrayList<>();
+
+        ChessPosition nextPos = getPositionFromVector(board, startPos, rowdx, 1, 1);
+        if (isInBounds(nextPos)) {
+            Collection<ChessMove> movesToAdd = getDiagnolPawnMove(board, startPos, new ChessPosition(nextPos.getRow(), nextPos.getColumn()));
+            if (movesToAdd != null) {
+                moves.addAll(movesToAdd);
+            }
+        }
+
+        nextPos = getPositionFromVector(board, startPos, rowdx, -1, 1);
+        if (isInBounds(nextPos)) {
+            Collection<ChessMove> movesToAdd = getDiagnolPawnMove(board, startPos, new ChessPosition(nextPos.getRow(), nextPos.getColumn()));
+            if (movesToAdd != null) {
+                moves.addAll(movesToAdd);
+            }
+        }
+
+        boolean spaceOneOpen = false;
+
+        nextPos = getPositionFromVector(board, startPos, rowdx, 0, 1);
+        if (isInBounds(nextPos)) {
+            Collection<ChessMove> movesToAdd = getStraightPawnMove(board, startPos, new ChessPosition(nextPos.getRow(), nextPos.getColumn()));
+            if (movesToAdd != null) {
+                moves.addAll(movesToAdd);
+                spaceOneOpen = true;
+            }
+        }
+
+        if (spaceOneOpen) {
+            nextPos = getPositionFromVector(board, startPos, rowdx, 0, 2);
+            if (startPos.getRow() == 7 && this.getTeamColor().equals(ChessGame.TeamColor.BLACK)) {
+                Collection<ChessMove> movesToAdd = getStraightPawnMove(board, startPos, new ChessPosition(nextPos.getRow(), nextPos.getColumn()));
+                if (movesToAdd != null) {
+                    moves.addAll(movesToAdd);
+                }
+            }
+            if (startPos.getRow() == 2 && this.getTeamColor().equals(ChessGame.TeamColor.WHITE)) {
+                Collection<ChessMove> movesToAdd = getStraightPawnMove(board, startPos, new ChessPosition(nextPos.getRow(), nextPos.getColumn()));
+                if (movesToAdd != null) {
+                    moves.addAll(movesToAdd);
+                }
+            }
+        }
+
+        return moves;
+    }
+
     /**
      * Calculates all the positions a chess piece can move to
      * Does not take into account moves that are illegal due to leaving the king in
@@ -53,8 +202,74 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
-    }
+        Collection<ChessMove> possibleMoves = new ArrayList<>();
+
+        switch (this.getPieceType()) {
+            case KING -> {
+                return getMovesInDirections(board, myPosition,1, new int[][] {
+                        {0,1},
+                        {1,1},
+                        {1,0},
+                        {1,-1},
+                        {0,-1},
+                        {-1,-1},
+                        {-1,0},
+                        {-1,1},
+                });
+            }
+            case QUEEN -> {
+                return getMovesInDirections(board, myPosition,8, new int[][] {
+                        {0,1},
+                        {1,1},
+                        {1,0},
+                        {1,-1},
+                        {0,-1},
+                        {-1,-1},
+                        {-1,0},
+                        {-1,1},
+                });
+            }
+            case BISHOP -> {
+                return getMovesInDirections(board, myPosition,8, new int[][] {
+                        {1,1},
+                        {1,-1},
+                        {-1,-1},
+                        {-1,1},
+                });
+            }
+            case ROOK -> {
+                return getMovesInDirections(board, myPosition,8, new int[][] {
+                        {0,1},
+                        {1,0},
+                        {0,-1},
+                        {-1,0},
+                });
+            }
+            case KNIGHT -> {
+                return getMovesInDirections(board, myPosition,1, new int[][] {
+                        {1,2},
+                        {2,1},
+                        {2,-1},
+                        {1,-2},
+                        {-1,-2},
+                        {-2,-1},
+                        {-2,1},
+                        {-1,2},
+                });
+            }
+            case PAWN -> {
+                int rowdx;
+                if (this.getTeamColor().equals(ChessGame.TeamColor.WHITE)) {
+                    rowdx = 1;
+                } else {
+                    rowdx = -1;
+                }
+
+                return getPawnMoves(board, myPosition, rowdx);
+            }
+        }
+        return possibleMoves;
+    };
 
     @Override
     public boolean equals(Object obj) {
