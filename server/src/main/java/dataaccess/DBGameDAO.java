3362683131ad127667349cc82070aca98950040c
Clay Eldredge <clay.eldredge@gmail.com>
2026-03-11 17:47:40 -0600

    Get and create user

--- server/src/main/java/dataaccess/DBGameDAO.java
+++ server/src/main/java/dataaccess/DBGameDAO.java
@@ -1,22 +1,23 @@
 package dataaccess;
 
+import chess.ChessGame;
 import model.GameData;
 
 public class DBGameDAO implements GameDAO{
     public GameData createGame(String gameName) throws DataAccessException {
-
+        return new GameData(1,"","","",new ChessGame());
     }
 
     public GameData getGame(int gameID) throws DataAccessException {
-
+        return new GameData(1,"","","",new ChessGame());
     }
 
     public GameData[] listGames() throws DataAccessException {
-
+        return new GameData[] {};
     }
 
     public GameData updateGame(int gameID, GameData gameData) throws DataAccessException {
-
+        return new GameData(1,"","","",new ChessGame());
     }
 
     public void clearAll() throws DataAccessException {
--- server/src/main/java/dataaccess/DBUserDAO.java
+++ server/src/main/java/dataaccess/DBUserDAO.java
@@ -1,14 +1,47 @@
 package dataaccess;
 
+import model.AuthData;
 import model.UserData;
 
 public class DBUserDAO implements UserDAO{
     public UserData getUser(String username) throws DataAccessException {
-        return new UserData("","","");
+        String sql = "SELECT username, password, email FROM user WHERE username=?";
+
+        try (var connection = DatabaseManager.getConnection()) {
+            try (var ps = connection.prepareStatement(sql)) {
+                ps.setString(1, username);
+
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return new UserData(
+                                rs.getString("username"),
+                                rs.getString("password"),
+                                rs.getString("email")
+                        );
+                    }
+                }
+                return null;
+            }
+        } catch (Exception e) {
+            throw new DataAccessException("Unable to get user", e);
+        }
     }
 
     public UserData createUser(UserData userData) throws DataAccessException {
-        return new UserData("","","");
+        String sql = "INSERT INTO user (username, password, email) VALUES (?, ?, ?)";
+
+        try (var connection = DatabaseManager.getConnection()) {
+            try (var ps = connection.prepareStatement(sql)) {
+                ps.setString(1, userData.username());
+                ps.setString(2, userData.password());
+                ps.setString(3, userData.email());
+
+                ps.executeUpdate();
+                return userData;
+            }
+        } catch (Exception e) {
+            throw new DataAccessException("Unable to create user", e);
+        }
     }
 
     public void clearAll() throws DataAccessException {
